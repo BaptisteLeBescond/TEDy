17e36475cd6a7f23ff97f9da81fde28849b814a8
BaptisteLeBescond <baptiste.lb@gmail.com>
2016-06-08 15:24:05 +0200

    Set Timezone

--- vendor/monolog/monolog/src/Monolog/Logger.php
+++ vendor/monolog/monolog/src/Monolog/Logger.php
@@ -308,7 +308,7 @@ class Logger implements LoggerInterface
         }
 
         if (!static::$timezone) {
-            static::$timezone = new \DateTimeZone(date_default_timezone_get() ?: 'UTC');
+            static::$timezone = new \DateTimeZone(date_default_timezone_get() ?: 'Europe/Paris');
         }
 
         if ($this->microsecondTimestamps) {
